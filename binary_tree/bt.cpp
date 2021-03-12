170c12798870fa6da4bcbc44be76314ecf672bc2
leeli0830 <leeli0830@gmail.com>
2021-03-12 01:44:43 -0800

    Binary Tree File 1 Is Updated

--- binary_tree/bt.cpp
+++ binary_tree/bt.cpp
@@ -2,6 +2,31 @@
 
 using namespace std;
 
+struct Btree
+{
+    Btree *lchild;
+    Btree *rchild;
+    int data;
+};
+
+Btree *head = NULL;
+// Btree *current = NULL;
+
+void insertNode(int num)
+{
+    Btree *newBtree = new Btree;
+
+    if (head == NULL)
+    {
+        newBtree->data = num;
+        head = newBtree;
+        // current = newBtree;
+    }
+    else if (head != NULL)
+    {
+        }
+}
+
 int main()
 {
     // Put test codes here
